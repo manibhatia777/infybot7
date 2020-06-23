c87be574f23371b4ca6f0d40f2e5f753722c5797
manibhatia777 <manibhatia777@gmail.com>
2020-06-23 18:25:54 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\infytestbot7-Web-Deploy.pubxml -p:Password=Ns7vXZ2Lo9vSYySu46jYysKDTTeScoBmzJihxrbpq0vac8mNPe3fCR8fHuxQ
+
